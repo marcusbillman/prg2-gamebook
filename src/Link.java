f5f96aa5c5aaba5133569447ba947c80922adc56
marcus.billman <54668482+marcusbillman@users.noreply.github.com>
2021-02-02 12:59:08 +0100

    Link, Model: Write Javadoc

--- src/Link.java
+++ src/Link.java
@@ -1,11 +1,20 @@
-// TODO: Write Javadoc
-
+/**
+ * A link in the gamebook that allows the player to progress to other pages.
+ */
 public class Link {
     private int id;
     private int fromPageId;
     private int toPageId;
     private String text;
 
+    /**
+     * Constructs a link with all properties defined
+     * @param id unique identifier for use in database
+     * @param fromPageId id of the page on which the link appears
+     * @param toPageId id of the page to which the link points
+     * @param text text that is displayed to the user and describes e.g. the
+     *             action that the gamebook character takes
+     */
     public Link(int id, int fromPageId, int toPageId, String text) {
         this.id = id;
         this.fromPageId = fromPageId;
@@ -13,34 +22,60 @@ public class Link {
         this.text = text;
     }
 
+    /**
+     * @return unique identifier for use in database
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @param id unique identifier for use in database
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * @return id of the page on which the link appears
+     */
     public int getFromPageId() {
         return fromPageId;
     }
 
+    /**
+     * @param fromPageId id of the page on which the link appears
+     */
     public void setFromPageId(int fromPageId) {
         this.fromPageId = fromPageId;
     }
 
+    /**
+     * @return id of the page to which the link points
+     */
     public int getToPageId() {
         return toPageId;
     }
 
+    /**
+     * @param toPageId id of the page to which the link points
+     */
     public void setToPageId(int toPageId) {
         this.toPageId = toPageId;
     }
 
+    /**
+     * @return text that is displayed to the user and describes e.g. the
+     * action that the gamebook character takes
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * @param text text that is displayed to the user and describes e.g. the
+     *             action that the gamebook character takes
+     */
     public void setText(String text) {
         this.text = text;
     }
--- src/Model.java
+++ src/Model.java
@@ -1,15 +1,24 @@
 import java.util.ArrayList;
 
-// TODO: Write Javadoc
-
+/**
+ * Model that acts as a data structure for the gamebook, containing its pages
+ * and links. Has methods for interfacing with this data.
+ */
 public class Model {
     private ArrayList<Page> pages;
     private int currentPageId;
 
+    /**
+     * Constructs a blank model
+     */
     public Model() {
         this.pages = new ArrayList<>();
     }
 
+    /**
+     * @param id page id to find
+     * @return the page with the given id, or null of none is found
+     */
     public Page getPage(int id) {
         for (Page page : this.pages) {
             if (page.getId() == id) return page;
@@ -17,24 +26,42 @@ public class Model {
         return null;
     }
 
+    /**
+     * Creates a new page and appends it to the model
+     * @param id unique identifier for use in database
+     * @return the newly created page
+     */
     public Page createPage(int id) {
         Page page = new Page(id);
         this.pages.add(page);
         return page;
     }
 
+    /**
+     * @return all pages in the model
+     */
     public ArrayList<Page> getPages() {
         return pages;
     }
 
+    /**
+     * @param pages pages to overwrite the model with
+     */
     public void setPages(ArrayList<Page> pages) {
         this.pages = pages;
     }
 
+    /**
+     * @return id of the page that is currently shown in the player or editor
+     */
     public int getCurrentPageId() {
         return currentPageId;
     }
 
+    /**
+     * @param currentPageId id of the page that is currently shown in the
+     *                      player or editor
+     */
     public void setCurrentPageId(int currentPageId) {
         this.currentPageId = currentPageId;
     }
