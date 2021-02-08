efe7004f2102535d9360f19f5dedfd8520762ffc
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-02-08 10:30:36 +0100

    Create DatabaseLoginDataExample class

---
+++ src/DatabaseLoginDataExample.java
@@ -0,0 +1,11 @@
+/**
+ * Example class containing the login credentials for the MySQL server. Make a copy of this class with the name
+ * DatabaseLoginData and fill in your credentials.
+ */
+public class DatabaseLoginDataExample {
+    public static String address = "";
+    public static String port = "";
+    public static String database = "";
+    public static String username = "";
+    public static String password = "";
+}
