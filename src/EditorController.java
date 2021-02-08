92b58774bfb55803c34b61ebf1e4735733bbc66d
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-02-08 11:02:16 +0100

    Create EditorController class

---
+++ src/EditorController.java
@@ -0,0 +1,18 @@
+/**
+ * MVC controller for the Gamebook Editor that handles all communication between EditorView (MVC view) and
+ * DatabaseModel (MVC model).
+ */
+public class EditorController {
+    private DatabaseModel databaseModel;
+    private final EditorView editorView;
+
+    /**
+     * Constructs the controller.
+     * @param databaseModel model that handles reading from and writing to a MySQL database
+     * @param editorView view for the Gamebook Editor app
+     */
+    public EditorController(DatabaseModel databaseModel, EditorView editorView) {
+        this.databaseModel = databaseModel;
+        this.editorView = editorView;
+    }
+}
