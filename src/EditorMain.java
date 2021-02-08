2b6099e021a0e4ecd1712a5765d3b85c566687a5
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-02-08 11:02:24 +0100

    Create EditorMain class

---
+++ src/EditorMain.java
@@ -0,0 +1,14 @@
+/**
+ * Contains the main() method which is the entry point for the Gamebook Editor app
+ */
+public class EditorMain {
+    /**
+     * Entry point for the Gamebook Editor app
+     * @param args unused arguments
+     */
+    public static void main(String[] args) {
+        DatabaseModel databaseModel = new DatabaseModel();
+        EditorView editorView = new EditorView("Gamebook Editor");
+        new EditorController(databaseModel, editorView);
+    }
+}
