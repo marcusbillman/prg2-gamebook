dfb42eb1253e8d689c99f1802cb3e97b9980be9d
marcus.billman <54668482+marcusbillman@users.noreply.github.com>
2021-02-16 13:46:35 +0100

    PlayerView: Design basic UI

--- src/PlayerView.java
+++ src/PlayerView.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 public class PlayerView {
     private final JFrame frame;
     private JPanel panel;
+    private JTextArea bodyTextArea;
+    private JPanel buttonsPanel;
 
     /**
      * Constructs a player view, packs the UI components and displays the
@@ -14,6 +16,8 @@ public class PlayerView {
         frame.setContentPane(panel);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         frame.pack();
+
+        this.buttonsPanel.setLayout(new BoxLayout(this.buttonsPanel, BoxLayout.Y_AXIS));
 
         frame.setVisible(true);
     }
